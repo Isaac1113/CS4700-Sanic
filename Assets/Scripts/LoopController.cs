c59c5ee34b64f6a5adb5ba22951b3c24a14aa7c7
Isaac Diaz <itdiaz@cpp.edu>
2023-03-13 23:32:30 -0700

    Add better loop mechanics

--- Assets/Scripts/LoopController.cs
+++ Assets/Scripts/LoopController.cs
@@ -6,15 +6,10 @@ public class LoopController : MonoBehaviour
 {
     [SerializeField] private MeshCollider entryLoop;
     [SerializeField] private MeshCollider exitLoop;
-    
-    [SerializeField] private Transform loopDisablerPos;
-    [SerializeField] private Transform SpotToMoveDisabler;
 
     private void OnTriggerEnter(Collider other)
     {
         entryLoop.enabled = false;
         exitLoop.enabled = true;
-
-        loopDisablerPos.position = SpotToMoveDisabler.position;
     }
 }
--- Assets/Scripts/SwapLoopController.cs
+++ Assets/Scripts/SwapLoopController.cs
@@ -8,11 +8,31 @@ public class SwapLoopController : MonoBehaviour
     [SerializeField] private MeshCollider leftLoop;
     [SerializeField] private GameObject loopDisabler;
 
+    // If loop is true, then the right loop was enabled and left loop disabled
+    // If loop is false, then the left loop was enabled and right loop disabled
+    bool loop;
+
     private void OnTriggerEnter(Collider other)
     {
-        leftLoop.enabled = true;
-        rightLoop.enabled = true;
+        Rigidbody otherRb = other.GetComponent<Rigidbody>();
 
-        loopDisabler.SetActive(true);
+        if(Mathf.Abs(otherRb.velocity.x) > 10f)
+        {
+            loop = rightLoop.enabled ? true : false;
+            leftLoop.enabled = true;
+            rightLoop.enabled = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(loop)
+        {
+            rightLoop.enabled = false;
+        }
+        else
+        {
+            leftLoop.enabled = false;
+        }
     }
 }
